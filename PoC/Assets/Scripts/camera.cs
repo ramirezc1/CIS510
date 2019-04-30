3df2828a5a15a0a58a2a67f44e0cb7b57b3b6ebc
Cristian Ramirez <cramire8@uoregon.edu>
2019-04-29 22:49:54 -0700

    camera position added

---
+++ PoC/Assets/Scripts/camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public GameObject car;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - car.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = car.transform.position + offset;
+    }
+}
+
+
