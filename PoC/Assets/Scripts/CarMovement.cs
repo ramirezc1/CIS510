40a75e948869a3e6046fb753712b9f31c27d8f89
Nithin Krishna <nithin@yoga.cs.uoregon.edu>
2019-05-01 08:31:02 -0700

    adding movement just started. Try to get the car to move intially. Later add acceleration and deceleration

---
+++ PoC/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;
+    public float m_Speed = 12f;
+    public float m_TurnSpeed = 180f;
+    public AudioSource m_MovementAudio;
+    public AudioClip m_EngineIdling;
+    public AudioClip m_Accelerataion;
+    public AudioClip m_Deceleration;
+    public float m_PitchRange = 0.2f;
+
+    private string m_MovementAxisName;
+    private string m_TurnAxisName;
+    private Rigidbody m_RigidBody;
+    private float m_MovementInputValue;
+    private float m_TurnInputValue;
+    private float m_OriginalPitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
