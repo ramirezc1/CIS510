907a6c698493be77b47650826ef20e37e1e0c953
Nithin Krishna <nithin@yoga.cs.uoregon.edu>
2019-05-03 02:04:11 -0700

    Basic car controls working. But not climbing on bridges

--- PoC/Assets/Scripts/CarMovement.cs
+++ PoC/Assets/Scripts/CarMovement.cs
@@ -22,18 +22,71 @@ public class CarMovement : MonoBehaviour
 
     private void Awake()
     {
-
+        // storing a reference to a particular gameobject or rigid body.
+        m_RigidBody = GetComponent<Rigidbody>();
     }
 
+    private void OnEnable()
+    {
+        m_RigidBody.isKinematic = false;
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f; //resetting input speed and turn 
+    }
+    private void OnDisable()
+    {
+        //when the car is turned off , it becomes kinematic it stops and all
+        // forces acting on it will be nullified
+        m_RigidBody.isKinematic = true;
+    }
     // Start is called before the first frame update
     void Start()
     {
+        //vetical and horizontal movement axis names set to Vertical1, Vertical2,
+        // Horizontal1, Horizontal2 for respective players in Edit-> ProjectSettings-> input tab.
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+        //m_MovementAudio is of type Audio Source of Unity
+        m_OriginalPitch = m_MovementAudio.pitch;
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //Vertical# and Horizontal# keyboard inputs assigned to m_MovementInputValue and m_TurnInputValue
+        m_MovementInputValue = Input.GetAxis(m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis(m_TurnAxisName);
+    }
+
+    private void FixedUpdate()
+    {
+        //Fixed update runs every physics step instead of updating in every visual frame
+        //rendered which is done in update function.
+        //Here we want to move the car and increase its speed linearly and turn linearly aswell
+        //Electric vehicles accelerate linearly upto a fixed speed
+        Move();
+        Turn();
+    }
+
+    private void Move()
+    {
+        // Vector in car's forward direction
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+        m_Speed = m_Speed + 0.1f;
+
+        // Apply this movement to the rigidbody's position.
+        m_RigidBody.MovePosition(m_RigidBody.position + movement);
+    }
+
+    private void Turn()
+    {
+        // calculating the turn speed
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        // Turning along the Y axis only
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        // applying the rotation to the car
+        m_RigidBody.MoveRotation(m_RigidBody.rotation * turnRotation);
     }
 }
