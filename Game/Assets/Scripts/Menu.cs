e9440d6bddf366487ec05707c86382242eac9435
Cristian Ramirez <cramire8@uoregon.edu>
2019-05-16 12:36:13 -0700

    main menu

---
+++ Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
