3ea9246e1bc7e039b24427c3de2c5d10f3b7100d
Priya Sheela Kudva <priya.kudva@wsu.edu>
2019-05-16 21:35:02 -0700

    Fixed bug for testing.

--- Game/Assets/Scripts/Checkpoint.cs
+++ Game/Assets/Scripts/Checkpoint.cs
@@ -17,25 +17,7 @@ public class Checkpoint : MonoBehaviour
 			return; //If it's not the player dont continue
 		
 
-		if (transform == Laps.checkpointA[Laps.currentCheckpoint].transform) 
-		{
-			//Check so we dont exceed our checkpoint quantity
-			if (Laps.currentCheckpoint + 1 < Laps.checkpointA.Length) 
-			{
-				//Add to currentLap if currentCheckpoint is 0
-				if(Laps.currentCheckpoint == 0)
-					Laps.currentLap++;
-				Laps.currentCheckpoint++;
-			} 
-			else 
-			{
-				//If we dont have any Checkpoints left, go back to 0
-				Laps.currentCheckpoint = 0;
-			}
-		}
-
 
 	}
 
-}
 }
