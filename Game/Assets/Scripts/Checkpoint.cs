7c3c85387655f0059327999d81a9234c5bf8e301
Priya Sheela Kudva <priya.kudva@wsu.edu>
2019-05-17 20:34:52 -0700

    Checkpoint logic established. Need help with transition back to menu.

--- Game/Assets/Scripts/Checkpoint.cs
+++ Game/Assets/Scripts/Checkpoint.cs
@@ -5,25 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class Checkpoint : MonoBehaviour
 {
-	public int laps;
-	private int checkpoints;
 
 	void  Start ()
 	{
-		checkpoints = 0;
+
 	}
 	
 	void  OnTriggerEnter ( Collider other  )
 	{
 		//Is it the Player who enters the collider?
-		if (other.CompareTag("Player")) {
-			checkpoints++;
-		}
-		if (checkpoints == laps){
-			GameObject.Find("Player").SendMessage("Finish");
-		}
+		if (!other.CompareTag("Player")) 
+			return; //If it's not the player dont continue
 
 
+		if (transform == Laps.checkpointA[Laps.currentCheckpoint].transform) 
+		{
+			//Check so we dont exceed our checkpoint quantity
+			if (Laps.currentCheckpoint + 1 < Laps.checkpointA.Length) 
+			{
+				//Add to currentLap if currentCheckpoint is 0
+				if(Laps.currentCheckpoint == 0)
+					Laps.currentLap++;
+				Laps.currentCheckpoint++;
+			} 
+			else 
+			{
+				//If we dont have any Checkpoints left, go back to 0
+				Laps.currentCheckpoint = 0;
+			}
+		}
+ 
+ 
 	}
 
 }
