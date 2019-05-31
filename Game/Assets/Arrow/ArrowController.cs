a625c291bddbbf79e3c2da835a741f78875fabc4
Priya Sheela Kudva <priya.kudva@wsu.edu>
2019-05-31 13:15:10 -0700

    Added arrows.

---
+++ Game/Assets/Arrow/ArrowController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+
+public class ArrowController : MonoBehaviour
+{
+    public Transform m_Targets;
+    //public Transform target;
+    public float speed = 5f;
+
+    void LateUpdate()
+    {
+        Vector3 direction = m_Targets.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+}
